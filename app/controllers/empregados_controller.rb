3e7062247534f47ebce3a1d990b34bc0661b40e7
Maria Luísa Câmara <120509865+malubritoc@users.noreply.github.com>
2024-06-08 00:45:29 +0000

    criando funcões

--- app/controllers/empregados_controller.rb
+++ app/controllers/empregados_controller.rb
@@ -1,2 +1,42 @@
 class EmpregadosController < ApplicationController
+    def create
+        @empregado = Empregado.new(empregado_params)
+        
+        if @empregado.save
+            render json: @empregado, status: :created
+        else
+            render json: @empregado.errors, status: :unprocessable_entity
+        end
+    end
+    
+    private
+    
+        def empregado_params
+        params.require(:empregado).permit(:nome, :cpf, :email)
+        end
+
+    def show
+        @empregado = Empregado.find(params[:id])
+        render json: @empregado
+    end
+
+    def index
+        @empregados = Empregado.all
+        render json: @empregados
+    end
+    
+    def update
+        @empregado = Empregado.find(params[:id])
+        
+        if @empregado.update(empregado_params)
+            render json: @empregado
+        else
+            render json: @empregado.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @empregado = Empregado.find(params[:id])
+        @empregado.destroy!
+      end
 end
